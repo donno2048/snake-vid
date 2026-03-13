b61a75dd0b29065f452f434cb7f5014a1142fd33
Elisha Hollander <just4now666666@gmail.com>
2026-03-13 15:19:37 +0200

    test CI

--- main.c
+++ main.c
@@ -23,12 +23,14 @@ int synth_cb(short *wav, int numsamples, espeak_EVENT *events) {
     if (wav && numsamples) {
         fwrite(wav, sizeof(short), numsamples, out_audio);
         samples += numsamples;
+        printf("%lld ", samples);
     }
     return 0;
 }
 
 void audio_wait() {
     espeak_Synchronize();
+    printf("  %lld  ", samples);
     long long ns = (1000000000LL * samples) / sample_rate;
     audio_end.tv_nsec += ns % 1000000000LL;
     audio_end.tv_sec += ns / 1000000000LL + audio_end.tv_nsec / 1000000000LL;
