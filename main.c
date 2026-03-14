bc459c08addc15dc55be19bdcdbed10a6b11229b
Elisha Hollander <just4now666666@gmail.com>
2026-03-14 17:20:24 +0200

    add minimum silence

--- main.c
+++ main.c
@@ -52,7 +52,7 @@ void _shell(const char *command, int wait_audio) {
         struct timespec now;
         struct timespec audio_end = audio_wait();
         clock_gettime(CLOCK_MONOTONIC, &now);
-        long long secs = now.tv_sec - audio_end.tv_sec;
+        long long secs = max(1, now.tv_sec - audio_end.tv_sec);
         short zero = 0;
         for(int i = 0; i < sample_rate * secs; i++)
             fwrite(&zero, sizeof(short), 1, out_audio);
