7eedd375ee4e823dd12bfbe01af7c66d2c77dc08
Elisha Hollander <just4now666666@gmail.com>
2026-03-15 15:35:29 +0200

    new script

--- main.sh
+++ main.sh
@@ -1,22 +1,23 @@
-#This video shows the extreme optimization possible using X86 assembly specifically using by showing a snake game I made, I believe is the smallest snake game ever written.
-#First, let's look at the code, I'll print it stripped from comment to make it easier to view.
+#This video shows the extreme level of optimization you can achieve with x86 assembly, by showcasing my fifty-four bytes snake game.
+#First, let's take a look at the code. I'll print it without the comments so it's easier to view.
 sed 's/\s*;.*//;/^$/d' snake.asm
-#Let's assemble the game using nasm.
+#Now let's assemble the game using NASM.
 nasm snake.asm -o snake.com
-#Here's the full hex dump of the game. As you can see, it's quite small.
+#Here's the full hex dump of the game. As you can see... it's very small.
 hexdump snake.com
-#So small, in fact, that it could fit in a single QR code.
+#In fact, it's so small that it can fit inside a single QR code.
 qrencode -8 -r snake.com -t UTF8 -m 2
-#Let's see how small it really is. To do so, let's make a C program that does nothing and contains only a declaration for the main method.
+#But how small is it really? To get some perspective, let's create a C program that does absolutely nothing, and only contains a declaration for the main method.
 echo "main;" > small.c
-#If we compile this program
+#Now let's compile it.
 gcc -w small.c -o small
-#And look how much it weighs, we can see it's around fifteen thousand bytes.
+#If we check the size of the compiled program, we can see it's around fifteen thousand bytes.
 ls -l small
-#This is because the compiler adds some things to every program it has to have in order to be able to run correctly, I won't go into the details as it would take another full video. If we try to make the compiler not include any additional data in the binary and compile, we will get the minimal size for a binary file the compiler is willing to create
+#This happens because the compiler automatically adds extra code that every program needs in order to run correctly. I won't go into the details here, since that would take an entire separate video.
+#But if we force the compiler to remove as much extra data as possible, we can reach the smallest binary it is willing to generate.
 gcc -Os -nostartfiles -nodefaultlibs --entry 0 -Wl,--strip-all -xc /dev/null -o small
-#Which is around nine thousand bytes
+#That ends up being around nine thousand bytes
 ls -l small
 #However, the program I made is only fifty-four bytes.
 ls -l snake.com
-#The project was inspired by MattKC's video, who also tried to make a snake game that fits in a QR code.
+#This project was inspired by a video from MattKC, who also tried to create a snake game that fits inside a QR code.
